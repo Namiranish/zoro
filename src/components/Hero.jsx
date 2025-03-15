1497e3f7b0ad4d92a87fb61d03838e5fe85b00e3
Namirah anis <namiranish@gmail.com>
2025-03-16 03:23:13 +0530

    created the nav and hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+// import img from "../assets/landing-img.jpg"
+import styles from './Hero.module.css'
+const Hero = () => {
+    return(
+        <>
+        <div className={styles.heroContainer}></div>
+        </>
+    )
+}
+
+export default Hero
